b90cbbc1037a98d7f1c387d2e41fe801cccf610f
Matthias Kuehlewein <github@kuehlewein.de>
2022-10-02 13:15:07 +0200

    LineSymbolSettings: Fix enabling/disabling in line symbol editor
    The 'mid symbol distance' field and label were not correctly disabled
    and enabled.
    Enabling them by increasing 'Mid symbols per spot' was not possible
    unless reentering the symbols setting dialog. If they were enabled,
    removing the mid symbol would not disable them.
    Adding both to mid_symbol_widget_list solves the issue.
    
    Alternatively it would have been possible to replace in
    mid_symbol_distance_edit->
    setEnabled(mid_symbol_distance_edit->isEnabled() && ... by
    setEnabled(!symbol->mid_symbol->isEmpty() && ...

--- src/gui/symbols/line_symbol_settings.cpp
+++ src/gui/symbols/line_symbol_settings.cpp
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later
  *
  * Copyright 2012, 2013 Thomas Schöps (OpenOrienteering)
- * Copyright 2012-2018 Kai Pastor (OpenOrienteering)
+ * Copyright 2012-2022 Kai Pastor (OpenOrienteering)
  *
  * This file is part of LibreMapper.
  */
@@ -240,6 +240,7 @@ LineSymbolSettings::LineSymbolSettings(LineSymbol* symbol, SymbolSettingDialog* 
 	mid_symbol_widget_list = {
 	    mid_symbol_placement_label, mid_symbol_placement_combo,
 	    mid_symbol_per_spot_label, mid_symbol_per_spot_edit,
+	    mid_symbol_distance_label, mid_symbol_distance_edit,
 	    show_at_least_one_symbol_check,
 	};
 	
