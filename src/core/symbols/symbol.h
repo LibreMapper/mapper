9a93f35d301484eb9269ef6fc6e5dccd48eb923a
Libor Pechacek <lpechacek@gmx.com>
2023-05-01 22:11:43 +0200

    OcdFileImport/Export: Maintain symbol preferred tool
    For the sake of enhanced OCD format compatibility, store
    the preferred tool setting in the OMAP file and save it again
    on OCD export. Mapper does not make use of the preferred tool
    information bit.

--- src/core/symbols/symbol.h
+++ src/core/symbols/symbol.h
@@ -516,6 +516,17 @@ public:
 	 */
 	virtual int getMinimumLength() const;
 	
+	/**
+	 * \see setOcdTool
+	 */
+	quint8 ocdPreferredTool() const { return ocd_preferred_tool; };
+
+	/**
+	 * setOcdTool has been introduced exclusively for enhandec OCD format
+	 * compatibility. It is not used by Mapper except for OCD file export.
+	 */	
+	void setOcdTool(quint8 tool) { ocd_preferred_tool = tool; };	
+
 protected:
 	/**
 	 * Sets the rotatability state of the symbol.
@@ -638,6 +649,7 @@ private:
 	bool is_hidden;           /// \see isHidden()
 	bool is_protected;        /// \see isProtected()
 	bool is_rotatable = false;
+	quint8 ocd_preferred_tool = 0; /// \see setOcdTool()
 };
 
 
