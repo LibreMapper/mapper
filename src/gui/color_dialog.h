4237e241f542e1bb948617b6032bf8a7bcbd344a
Libor Pechacek <lpechacek@gmx.com>
2023-09-10 18:35:19 +0200

    ColorDialog: Allow symbol editing from the color usage tab
    This change allows users open symbol edit dialog directly
    from the color usage list.

--- src/gui/color_dialog.h
+++ src/gui/color_dialog.h
@@ -87,6 +87,8 @@ protected slots:
 	
 	void setColorModified() { setColorModified(true); }
 	
+	void colorUsageItemClicked(QListWidgetItem* clicked_item);	
+	
 protected:
 	void setColorModified(bool modified);
 	
