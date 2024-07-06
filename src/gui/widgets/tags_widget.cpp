ce60b02672e5be06a7f05aeb9c9954826e725924
Matthias Kuehlewein <github@kuehlewein.de>
2024-07-06 21:28:37 +0200

    TagsWidget: Fix tag editor setup
    The 'help' button was not connected to actually show the related help
    content.
    When setting the margins around the 'help' button, the top margin
    parameter was accidentally set to QStyle::PM_LayoutLeftMargin instead.
    Now using 0 as in ColorListWidget.

--- src/gui/widgets/tags_widget.cpp
+++ src/gui/widgets/tags_widget.cpp
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later
  *
- * Copyright 2013-2020 Kai Pastor (OpenOrienteering)
+ * Copyright 2013-2024 Kai Pastor (OpenOrienteering)
  *
  * This file is part of LibreMapper.
  */
@@ -57,7 +57,7 @@ TagsWidget::TagsWidget(Map* map, MapView* main_view, MapEditorController* contro
 	QStyleOption style_option(QStyleOption::Version, QStyleOption::SO_DockWidget);
 	all_buttons_layout->setContentsMargins(
 		style()->pixelMetric(QStyle::PM_LayoutLeftMargin, &style_option) / 2,
-		style()->pixelMetric(QStyle::PM_LayoutLeftMargin, &style_option) / 2,
+		0, // Covered by the main layout's spacing.
 		style()->pixelMetric(QStyle::PM_LayoutRightMargin, &style_option) / 2,
 		style()->pixelMetric(QStyle::PM_LayoutBottomMargin, &style_option) / 2
 	);
@@ -69,6 +69,8 @@ TagsWidget::TagsWidget(Map* map, MapView* main_view, MapEditorController* contro
 	setLayout(layout);
 	
 	connect(tags_table, &QTableWidget::cellChanged, this, &TagsWidget::cellChange);
+	
+	connect(help_button, &QAbstractButton::clicked, this, &TagsWidget::showHelp);
 	
 	connect(map, &Map::objectSelectionChanged, this, &TagsWidget::objectTagsChanged);
 	connect(map, &Map::selectedObjectEdited, this, &TagsWidget::objectTagsChanged);
