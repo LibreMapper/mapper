d33a68899de9583df8eb57d094ceb0bd9a5baf3d
Kai Pastor <dg0yt@darc.de>
2025-05-12 22:16:53 +0200

    DrawPathTool: Fix crash
    Amends 340e506.

--- src/tools/draw_path_tool.cpp
+++ src/tools/draw_path_tool.cpp
@@ -1159,7 +1159,8 @@ void DrawPathTool::updateDashPointDrawing()
 		return;
 	
 	// Auto-activate dash points depending on if the selected symbol has a dash symbol.
-	draw_dash_points = editor->activeSymbol()->containsDashSymbol();
+	const auto* symbol = editor->activeSymbol();
+	draw_dash_points = symbol ? symbol->containsDashSymbol() : false;
 	if (dash_points_button)
 		dash_points_button->setChecked(draw_dash_points);
 }
