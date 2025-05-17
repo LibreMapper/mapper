4d7b13f75496da7c897981b3223032b220fdf05f
Matthias Kühlewein <github@kuehlewein.de>
2025-05-17 13:08:41 +0200

    MapFindFeature: Exclude hidden/protected objects (GH-2371)
    The 'Find next' search failed if the object query returned an object
    with either a hidden or protected symbol since these kind of objects
    can't be added to an object selection. This change excludes such
    objects from the result set. A test is added.
    
    ---------
    
    Co-authored-by: Kai Pastor <dg0yt@darc.de>
    Ported-to-LibreMapper: Libor Pechacek <lpechacek@gmx.com>

--- test/tools_t.cpp
+++ test/tools_t.cpp
@@ -1,7 +1,8 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later
  *
  * Copyright 2012, 2013 Thomas Schöps (OpenOrienteering)
- * Copyright 2015-2020 Kai Pastor (OpenOrienteering)
+ * Copyright 2015-2020, 2025 Kai Pastor (OpenOrienteering)
+ * Copyright 2025 Matthias Kühlewein (OpenOrienteering)
  *
  * This file is part of LibreMapper.
  */
@@ -23,10 +24,13 @@
 #include "core/map_color.h"
 #include "core/map_coord.h"
 #include "core/objects/object.h"
+#include "core/objects/object_query.h"
 #include "core/symbols/line_symbol.h"
+#include "core/symbols/point_symbol.h"
 #include "global.h"
 #include "gui/main_window.h"
 #include "gui/map/map_editor.h"
+#include "gui/map/map_find_feature.h"
 #include "gui/map/map_widget.h"
 #include "templates/paint_on_template_feature.h"
 #include "tools/edit_point_tool.h"
@@ -149,7 +153,7 @@ void TestMapEditor::simulateDrag(const QPointF& start_pos, const QPointF& end_po
 }
 
 
-// ### TestTools ###
+// ### ToolsTest ###
 
 void ToolsTest::initTestCase()
 {
@@ -214,6 +218,60 @@ void ToolsTest::paintOnTemplateFeature()
 	QCOMPARE(PaintOnTemplateFeature::roundToMultiple(347,  20), 340);
 	QCOMPARE(PaintOnTemplateFeature::roundToMultiple(347,  50), 350);
 	QCOMPARE(PaintOnTemplateFeature::roundToMultiple(347, 100), 300);
+}
+
+
+void ToolsTest::testFindObjects()
+{
+	auto* map = new Map;
+	{
+		auto* normal_point_symbol = new PointSymbol();
+		map->addSymbol(normal_point_symbol, 0);
+		
+		auto* hidden_point_symbol = new PointSymbol();
+		hidden_point_symbol->setHidden(true);
+		map->addSymbol(hidden_point_symbol, 1);
+		
+		auto* protected_point_symbol = new PointSymbol();
+		protected_point_symbol->setProtected(true);
+		map->addSymbol(protected_point_symbol, 2);
+		
+		auto add_object = [map](Symbol* symbol, const char* label) {
+			auto* object = new PointObject(symbol);
+			object->setTag(QLatin1String("match"), QLatin1String(label));
+			map->addObject(object);
+		};
+		add_object(normal_point_symbol, "yes");     // expected match
+		add_object(normal_point_symbol, "no");
+		add_object(normal_point_symbol, "yes");     // expected match
+		add_object(hidden_point_symbol, "yes");
+		add_object(normal_point_symbol, "yes");     // expected match
+		add_object(protected_point_symbol, "yes");
+	}
+	
+	TestMapEditor editor(map);  // taking ownership
+	
+	ObjectQuery query {QLatin1String("match"), ObjectQuery::OperatorIs, QLatin1String("yes")};
+	QVERIFY(query);
+	
+	MapFindFeature::findAllMatchingObjects(*editor.editor, query);
+	QCOMPARE(map->getNumSelectedObjects(), 3);
+	
+	MapFindFeature::findNextMatchingObject(*editor.editor, query);
+	QCOMPARE(map->getNumSelectedObjects(), 1);
+	auto* first_match = map->getFirstSelectedObject();
+	
+	MapFindFeature::findNextMatchingObject(*editor.editor, query);
+	QCOMPARE(map->getNumSelectedObjects(), 1);
+	QVERIFY(map->getFirstSelectedObject() != first_match);
+	
+	MapFindFeature::findNextMatchingObject(*editor.editor, query);
+	QCOMPARE(map->getNumSelectedObjects(), 1);
+	QVERIFY(map->getFirstSelectedObject() != first_match);
+	
+	MapFindFeature::findNextMatchingObject(*editor.editor, query);
+	QCOMPARE(map->getNumSelectedObjects(), 1);
+	QVERIFY(map->getFirstSelectedObject() == first_match);
 }
 
 
