04d776545d96ae8cc08ab9f41520cbdf5daae5e8
Kai Pastor <dg0yt@darc.de>
2025-05-19 07:45:19 +0200

    MapCoordF: Declare copy assignment operator
    Ported-to-LibreMapper: Libor Pechacek <lpechacek@gmx.com>

--- src/core/map_coord.h
+++ src/core/map_coord.h
@@ -539,6 +539,9 @@ public:
 	
 	
 	/** Assignment operator. */
+	constexpr MapCoordF& operator= (const MapCoordF& point) noexcept = default;
+	
+	/** Assignment operator. */
 	MapCoordF& operator= (const QPointF& point) noexcept;
 	
 	
