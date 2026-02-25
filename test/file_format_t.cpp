00f83059f320da2f53fe8aa3e27581a2782262d3
Libor Pechacek <lpechacek@gmx.com>
2026-02-25 15:49:33 +0100

    file_format_t: Adjust tests to LibreMapper
    Add OCD v2018 to testing and handle creation and modification date tags.

--- test/file_format_t.cpp
+++ test/file_format_t.cpp
@@ -1879,6 +1879,7 @@ void FileFormatTest::ocdAreaSymbolTest_data()
 	    { 10, "OCD10" },
 	    { 11, "OCD11" },
 	    { 12, "OCD12" },
+	    { 2018, "OCD2018" },
 	};
 	for (auto& t : tests)
 	{
@@ -1983,6 +1984,14 @@ void FileFormatTest::ocdAreaSymbolTest()
 			// Mapper may store rotation even if there are no rotatable patterns (left).
 			if (!expected_symbol.hasRotatableFillPattern())
 				expected_path.setRotation(0);
+
+			// Transfer the creation date and modification date tags
+			for (auto* tag_name : {"ctime", "mtime"})
+			{
+				auto const value = actual_object.getTag(QLatin1StringView(tag_name));
+				if (!value.isEmpty())
+					expected_path.setTag(QLatin1StringView(tag_name), value);
+			}
 		}
 		
 		// Verifying object property; symbol is for tracing
@@ -2001,6 +2010,7 @@ void FileFormatTest::ocdTextSymbolTest_data()
 	    { 10, "OCD10" },
 	    { 11, "OCD11" },
 	    { 12, "OCD12" },
+	    { 2018, "OCD2018" },
 	};
 	for (auto& t : tests)
 	{
